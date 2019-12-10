c389dfcf93d9ee8110d9fc024d3c7e9b5d3e1685
Daniel Nekrasov <nekrasov.danny@gmail.com>
2019-12-11 00:28:36 +0500

    move to es6 class

--- index.js
+++ index.js
@@ -1,61 +1,49 @@
-let data = {
-    prop1: "Hello",
-    prop2: "World"
-};
+import Dep from './src/Dep.js';
 
-let target = null;
-
-// Dependency class
-function Dep() {
-    this.subscribers = [];
-}
-
-Dep.prototype.depend = function () {
-    if (target && !this.subscribers.includes(target)) {
-        this.subscribers.push(target);
+function Store(props) {
+    const { data, el } = props;
+    // Watcher function
+    function watcher(myTargetFn) {
+        Dep.target = myTargetFn;
+        Dep.target();
+        Dep.target = null;
     }
-};
 
-Dep.prototype.notify = function () {
-    this.subscribers.forEach(sub => sub());
-};
+    Object.keys(data).forEach(key => {
+        let currentValue = data[key];
+        const dep = new Dep();
 
-// Watcher function
-function watcher(myTargetFn) {
-    target = myTargetFn;
-    target();
-    target = null;
+        Object.defineProperty(data, key, {
+            get: function () {
+                dep.depend();
+                return currentValue;
+            },
+            set: function (newValue) {
+                currentValue = newValue;
+                dep.notify();
+            }
+        });
+    });
+
+    Object.keys(data).forEach(key => {
+        const inputElem = document.querySelector(`${el} input[name="data.${key}"]`);
+
+        if (inputElem) {
+            watcher(() => {
+                inputElem.value = data[key];
+            })
+
+            inputElem.addEventListener('change', (event) => {
+                data[key] = event.currentTarget.value
+            })
+        }
+    })
 }
 
-Object.keys(data).forEach(key => {
-    let currentValue = data[key];
-    const dep = new Dep();
-    console.log(dep);
-
-    Object.defineProperty(data, key, {
-        get: function () {
-            dep.depend();
-            return currentValue;
-        },
-        set: function (newValue) {
-            currentValue = newValue;
-            dep.notify();
-        }
-    });
-});
-
-
-Object.keys(data).forEach(key => {
-    const el = document.querySelector(`#form input[name="data.${key}"]`);
-
-    if (el) {
-        watcher(() => {
-            el.value = data[key];
-        })
-
-        el.addEventListener('change', (event) => {
-            data[key] = event.currentTarget.value
-        })
+const store = new Store({
+    el: '#form',
+    data: {
+        prop1: "Hello",
+        prop2: "World"
     }
 })
-
