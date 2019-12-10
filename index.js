b0c16e227dd6aec5f00721a39ca4ac47fdb0eaab
Daniel <DanielNekrasov@users.noreply.github.com>
2019-12-10 11:28:02 +0500

    add getters & setters

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-// Dependency class
-function Dep(){
-    this.subscribers = [];
-}
-
-Dep.prototype.depend = function() {
-    if(target && !this.subscribers.includes(target)){
-        this.subscribers.push(target);
-    }
-}
-
-Dep.prototype.notify = function() {
-    this.subscribers.forEach(sub => sub())
+let data = {
+    quantity: 5,
+    price: 10
 }
 
 let target = null;
 
+let total = 0;
+let sale = 0;
+
+// Dependency class
+function Dep() {
+    this.subscribers = [];
+}
+
+Dep.prototype.depend = function () {
+    if (target && !this.subscribers.includes(target)) {
+        this.subscribers.push(target);
+    }
+}
+
+Dep.prototype.notify = function () {
+    this.subscribers.forEach(sub => sub())
+}
+
+
 // Watcher function
-function watcher(myTargetFn){
+function watcher(myTargetFn) {
     target = myTargetFn
-    dep.depend();
     target();
     target = null;
 }
 
 
-let quantity = 5;
-let price = 10;
-let total = 0;
+Object.keys(data).forEach(key => {
+    let currentValue = data[key]
+    const dep = new Dep();
 
-const dep = new Dep();
-watcher(() => total = quantity * price)
+    Object.defineProperty(data, key, {
+        get: function () {
+            dep.depend();
+            return currentValue;
+        },
+        set: function (newValue) {
+            currentValue = newValue;
+            dep.notify();
+        }
+    });
 
-console.log(total);
-quantity = 10;
-dep.notify();
-console.log(total);
+})
+
+watcher(() => {
+    total = data.price * data.quantity;
+})
+watcher(() => {
+    sale = data.price - data.price * 0.10;
+})
+
+console.log(sale);
+data.price = 100;
+console.log(sale);
